9fd2b04482836149e8e3b2ce5e992edecd53ead0
martinmazas <mazasmartin@gmail.com>
2020-12-29 22:30:52 +0200

    conection html node done, need to check allocation

--- js/handler.js
+++ js/handler.js
@@ -1,11 +1,9 @@
 const service_url = 'http://localhost:3000/api/plants';
-
 //Self invoke  for the button listeners
-;
 
 // logics of the buttons.
 (function userOperationsListeners() {
-    $("#get-query-search").click(() =>{
+    $("#get-query-search").click(() => {
         const queryValue = $("#query-value").val();
         const query = `?name=${queryValue}`;
         getAllPlantsBySearch(query);
@@ -15,60 +13,38 @@ const service_url = 'http://localhost:3000/api/plants';
 
 function getAllPlantsBySearch(query) {
     // alert('getAll');
-    // alert(service_url + query);
+    // console.log(service_url + query);
     $.ajax({
         url: service_url + query,
         type: 'GET',
         success: function (plants) {
-            console.log(plants);
             recreatePlantsTable(plants);
         }
     });
+
 }
 
 
-
+//Need to allocate the cards
 function recreatePlantsTable(plants) {
-    // console.log(plants);
-    // EXTRACT VALUE FOR HTML HEADER. 
-    let col = [];
-    for (let i = 0; i < plants.length; i++) {
-        for (let key in plants[i]) {
-            if (col.indexOf(key) === -1) {
+    const plantLen = plants.length;
+    if (plantLen) {
+        $("#table-data").append("<div id='multi-item-example' class='carousel slide carousel-multi-item' data-ride='carousel'>" +
+            "<ol class='carousel-indicators'><li data-target='#multi-item-example' data-slide-to='0' class='active'></li>" +
+            "<li data-target='#multi-item-example' data-slide-to='1'></li></ol><div class='carousel-inner-search' role='listbox'></div></div>");
 
-                col.push(key);
-            }
+        for (let i = 0; i < plantLen; i++) {
+            let row = "<div class='carousel-item active'><div id='card-location' class='col-md-3' style='float: left'>" +
+                "<div class='card mb-2'>" +
+                "<img class='card-img-top' src='$imgDb' alt='Card image cap'/>" +
+                "<div class='card-body'> <h4 class='card-title'>$nameDb</h4>" +
+                "<p class='card-text'>Level: Hard , Location: Indoor</p>" +
+                "<div id='btns'><a class='btn btn-light'>View</a>" +
+                "<a class='btn btn-light'>Add</a></div></div></div></div></div>"
+            row = row.replace("$imgDb", plants[i].image_url);
+            row = row.replace("$nameDb", plants[i].name);
+            $(".carousel-inner-search").append(row);
         }
     }
 
-    // col.splice(9, 1);
-    // col.splice(0, 1);
-
-    // CREATE DYNAMIC TABLE.
-    let table = document.createElement("table");
-
-    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-    let tr = table.insertRow(-1);// TABLE ROW.
-    console.log(tr);
-
-    for (let i = 0; i < col.length; i++) {
-        let th = document.createElement("th");// TABLE HEADER.
-        th.innerHTML = col[i];
-        tr.appendChild(th);
-    }
-
-    // ADD JSON DATA TO THE TABLE AS ROWS.
-    for (let i = 0; i < plants.length; i++) {
-        tr = table.insertRow(-1);
-
-        for (let j = 0; j < col.length; j++) {
-
-            let tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = plants[i][col[j]];
-        }
-    }
-    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-    const divContainer = document.getElementById("showData");
-    divContainer.innerHTML = "";
-    divContainer.appendChild(table);
 }
