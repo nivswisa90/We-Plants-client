500283c1ee22d21fe1aec3f0784b138998bb5522
martinmazas <mazasmartin@gmail.com>
2020-12-31 11:01:33 +0200

    adding weather API

--- js/handler.js
+++ js/handler.js
@@ -1,10 +1,12 @@
 const plants_service_url = "http://localhost:3000/api/plants";
 const user_service_url = "http://localhost:3000/api/users/2";
+const weather_url = "http://localhost:3000/api/weather";
 //Self invoke  for the button listeners
 
 // logics of the buttons.
 (function userOperationsListeners() {
     getMyFavoritesPlants();
+    getWeather();
     $("#get-query-search").click(() => {
         const queryValue = $("#query-value").val();
         const query = `?name=${queryValue}`;
@@ -12,7 +14,6 @@ const user_service_url = "http://localhost:3000/api/users/2";
     });
     $(document.body).on('click', '.btn-light', function(){
         const queryValue = $(this).val();
-        console.log(queryValue);
         const query = `?name=${queryValue}`;
         addToMyFavorites(query, queryValue);
     });
@@ -39,7 +40,6 @@ function getMyFavoritesPlants() {
 }
 
 function addToMyFavorites(query, name) {
-    console.log(query, name);
     $.ajax({
         url: user_service_url + query,
         type: "POST",
@@ -50,6 +50,41 @@ function addToMyFavorites(query, name) {
     })
 }
 
+function getWeather() {
+    $.ajax({
+        url: weather_url,
+        type: "GET",
+        success: function(weather) {
+            showWeather(weather);
+        } 
+    })
+}
+
+function showWeather(weather){
+    const celcius = Math.round(parseFloat(weather.main.temp)-273.15);
+    const fahrenheit = Math.round(((parseFloat(weather.main.temp)-273.15)*1.8)+32); 
+    
+    $('#description').html(weather.weather[0].description); 
+    $('#temp').html(celcius +  '&deg;');
+    $('#location').html(weather.name); 
+
+    // if( weather.weather[0].description.indexOf('rain') > 0 ) {
+    //     document.body.className = 'rainy';
+    // } else if( weather.weather[0].description.indexOf('cloud') > 0 ) {
+    //     document.body.className = 'cloudy';
+    // } else if( weather.weather[0].description.indexOf('sunny') > 0 ) {
+    //     document.body.className = 'sunny';
+    // }
+
+}
+
+function prepareDivOfCards(section) {
+    $(`#show${section}`).append(
+        `<div id='multi-item-example-${section}' class='carousel slide carousel-multi-item' data-ride='carousel'>` +
+            `<ol class='carousel-indicators'><li data-target='#multi-item-example-${section}' data-slide-to='0' class='active'></li>` +
+            `<li data-target='#multi-item-example-${section}' data-slide-to='1'></li></ol><div class='carousel-inner' role='listbox'><div id='${section}-carousel-item-active' class='carousel-item active'></div><div id='${section}-carousel-item' class='carousel-item'></div></div></div>`
+    );
+}
 
 
 function favoritesTable(users) {
@@ -62,19 +97,17 @@ function favoritesTable(users) {
             "<li data-target='#multi-item-example-favorites' data-slide-to='1'></li></ol><div class='carousel-inner' role='listbox'><div id='favorites-carousel-item-active' class='carousel-item active'></div><div id='favorites-carousel-item' class='carousel-item'></div></div></div>"
         );
         for (let i = 0; i < userLen; i++) {
-            console.log(users.myFavorites[i].image_url);
             let row =
                 "<div class='col-md-3' style='float: left'>" +
                 "<div class='card mb-2'>" +
                 `<img class='card-img-top' src='${users.myFavorites[i].image_url}' alt='Card image cap'/>` +
                 `<div class='card-body'> <h4 class='card-title'>${users.myFavorites[i].plant_name}</h4>` +
                 "<p class='card-text'>Level: Hard , Location: Indoor</p>" +
-                "<div id='btns'><button class='btn btn-info'>View</button> " +
+                "<div id='btns'><button class='btn btn-info'>Delete</button> " +
                 `<button class='btn btn-light' value='${users.myFavorites[i].plant_name}'>Add</button></div></div></div></div>`;
             if (i < 3) {
                 $("#favorites-carousel-item-active").append(row);
             } else {
-                $();
                 $("#favorites-carousel-item").append(row);
             }
         }
@@ -115,3 +148,4 @@ function recreatePlantsTable(plants) {
         $("#possible-error").show();
     }
 }
+
